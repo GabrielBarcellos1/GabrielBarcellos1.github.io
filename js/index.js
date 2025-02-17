48c5b60b01fa5dded082b518f1f5c60eaa9a0c23
Gabriel Barcellos <102869583+GabrielBarcellos1@users.noreply.github.com>
2025-02-17 12:05:55 -0300

    Update index.js
    Alteração descrição Items

--- js/index.js
+++ js/index.js
@@ -29,49 +29,49 @@ next.addEventListener('mouseout', sairNext)
 
 //DECLARANDO FUNCTIONS
 function entrarHtml(){
-evento.innerText = 'teste123'
+evento.innerText = 'O HelpDesk é responsável por fornecer suporte técnico e solucionar problemas de hardware e software para os usuários finais, garantindo a continuidade do funcionamento das operações de TI.'
 }
 function sairHtml(){
 evento.innerText = "Passe o cursor no card para ler"
 }
 function entrarCss(){
-evento.innerText = '11111111111111111'
+evento.innerText = 'Kaspersky é um software de segurança cibernética que oferece proteção contra vírus, malware, ransomwares e outras ameaças digitais, garantindo a segurança de dispositivos e dados.'
 }
 function sairCss(){
 evento.innerText = "Passe o cursor no card para ler"
 }
 function entrarJs(){
-evento.innerText = 'Com o Javascript as possibilidades são imensas, validar formularios, processar dados, manipular elementos na tela, consultar APIs, etc.'
+evento.innerText = 'CrowdStrike é uma plataforma de segurança baseada em nuvem que protege dispositivos e redes contra ameaças cibernéticas avançadas, como ataques de malware e ransomware, utilizando inteligência artificial e análise em tempo real.'
 }
 function sairJs(){
 evento.innerText = "Passe o cursor no card para ler"
 }
 function entrarC(){
-evento.innerText = 'O React é uma biblioteca Javascript desenvolvida pelos engenheiros do Facebook com o foco em criar interfaces de usuário. É atualmente a biblioteca mais requisitada no mercado.'
+evento.innerText = 'ZohoAssist é uma ferramenta de suporte remoto que permite aos técnicos acessarem e resolverem problemas nos dispositivos dos usuários de forma segura, facilitando o atendimento e a assistência técnica à distância.'
 }
 function sairC(){
 evento.innerText = "Passe o cursor no card para ler"
 }
 function entrarSass(){
-evento.innerText = 'O SASS é uma linguagem de extensão ao CSS, com ele vamos simplificar a escrita de CSS, além de ganhar mais dinamismo em nossas folhas de estil'
+evento.innerText = 'ManagementEngine é um conjunto de ferramentas de gerenciamento de TI que oferece soluções para monitoramento de rede, gerenciamento de dispositivos, automação de tarefas e segurança, com foco na eficiência operacional.'
 }
 function sairSass(){
 evento.innerText = "Passe o cursor no card para ler"
 }
 function entrarPython(){
-evento.innerText = 'Redux é uma biblioteca JavaScript para gerenciar o estado da aplicação é similar e inspirada pela arquitetura Flux do Facebook, ela pode ser utilizada com o React ou outras bibliotecas JavaScript.'
+evento.innerText = 'Active Directory é um serviço de diretório da Microsoft que facilita a administração de usuários e dispositivos em uma rede, garantindo a autenticação e controle de acesso com base em políticas organizacionais.'
 }
 function sairPython(){
 evento.innerText = "Passe o cursor no card para ler"
 }
 function entrarMaterial(){
-evento.innerText = 'Biblioteca de componentes React para um desenvolvimento ágil e fácil. O Material.ui é usado por grande empresas como Nasa, Walmart, Netflix, Amazon, Spotify, entre outras.'
+evento.innerText = 'Office 365 é uma plataforma de produtividade em nuvem da Microsoft que oferece ferramentas como Word, Excel, PowerPoint e Outlook, além de armazenamento em nuvem e colaboração em tempo real.'
 }
 function sairMaterial(){
 evento.innerText = "Passe o cursor no card para ler"
 }
 function entrarNext(){
-evento.innerText = 'O Next.js é o framework mais adotado pela comunidade do React. O seu maior objetivo é tornar nossa aplicação React mais performática e permitir a indexação do conteúdo da página pelos motores de busca.'
+evento.innerText = 'Azure é a plataforma de computação em nuvem da Microsoft, que oferece serviços de infraestrutura, desenvolvimento de aplicativos, armazenamento e inteligência artificial, ajudando empresas a gerenciar e escalar suas operações na nuvem.'
 }
 function sairNext(){
 evento.innerText = "Passe o cursor no card para ler"
